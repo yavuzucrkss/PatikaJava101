ac2194f77838a5eb2c300bdb7d2ef8fd0ce912ad
Yavuz Ucarkus <81562942+yavuzucrkss@users.noreply.github.com>
2024-06-29 21:24:40 +0300

    HW4

---
+++ SQLQueries/HW4.sql
@@ -0,0 +1,10 @@
+SELECT DISTINCT replacement_cost FROM film
+
+SELECT COUNT(DISTINCT replacement_cost) FROM film
+
+SELECT COUNT(DISTINCT(title)) FROM film WHERE title LIKE  'T%' and rating = 'G'
+
+SELECT COUNT(DISTINCT country) FROM country WHERE country LIKE '_____'
+
+SELECT COUNT(DISTINCT city) FROM city WHERE city LIKE 'R%r'
+
